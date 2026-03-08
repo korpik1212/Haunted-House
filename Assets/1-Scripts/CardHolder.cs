6c88e890f54eeab053ff7e3e3c2925a832d3223f
Mehmet Koray Yılmaz <korpik123456@gmail.com>
2026-03-08 22:50:38 +0100

    card anims (needs bit more work)

--- Assets/1-Scripts/CardHolder.cs
+++ Assets/1-Scripts/CardHolder.cs
@@ -35,32 +35,49 @@ public class CardHolder : MonoBehaviour
     }
 
 
+    public float spacing = 150f;
+    public float maxRotation = 15f;
+    public float heightArc = 20f;
+    public float lerpSpeed = 12f;
+
     private void Update()
     {
         UpdateCardPositions();
-        UpdateCardRotations();
     }
-
-
 
     public void UpdateCardPositions()
     {
-
-        float spacing = 150f;
-
         for (int i = 0; i < cards.Count; i++)
         {
+            float normalizedPos = (cards.Count > 1) ? (i / (float)(cards.Count - 1)) * 2f - 1f : 0f;
+
             float xPos = (i - (cards.Count - 1) / 2f) * spacing;
-            cards[i].transform.localPosition = new Vector3(xPos, 0f, 0f);
+            float yPos = -Mathf.Abs(normalizedPos) * heightArc;
+            float zRot = -normalizedPos * maxRotation;
+
+            Vector3 targetPos = new Vector3(xPos, yPos, 0f);
+            Quaternion targetRot = Quaternion.Euler(0f, 0f, zRot);
+            Vector3 targetScale = Vector3.one;
+
+            bool isHovered = cards[i].isHovering;
+
+            if (isHovered)
+            {
+                targetPos.y += 50f;
+                targetRot = Quaternion.identity;
+                targetScale = Vector3.one * 1.2f;
+                cards[i].transform.SetAsLastSibling();
+            }
+            else
+            {
+                cards[i].transform.SetSiblingIndex(i);
+            }
+
+            cards[i].transform.localPosition = Vector3.Lerp(cards[i].transform.localPosition, targetPos, Time.deltaTime * lerpSpeed);
+            cards[i].transform.localRotation = Quaternion.Lerp(cards[i].transform.localRotation, targetRot, Time.deltaTime * lerpSpeed);
+            cards[i].transform.localScale = Vector3.Lerp(cards[i].transform.localScale, targetScale, Time.deltaTime * lerpSpeed);
         }
-
     }
-
-    public void UpdateCardRotations()
-    {
-
-    }
-
 
 
 }
