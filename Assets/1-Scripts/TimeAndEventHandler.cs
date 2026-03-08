56fc5bc488aea7606b08014ab0c02e519cfed33d
Emily Hlíf Carozzini <emilycarozzini@gmail.com>
2026-03-08 21:09:14 +0100

    added TimeAndEventHandler class

---
+++ Assets/1-Scripts/TimeAndEventHandler.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TimeAndEventHandler : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void generateEvent(ScareCard scareCard, EnvironmentElement environmentElement)
+    {
+        
+    }
+}
