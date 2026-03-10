526349b77eb10585c67072b8e487c79b8ded7cc0
Eskarina Ulrich <eskarina.ulrich@1und.de>
2026-03-10 13:49:24 +0100

    add logic to humans alkinhg from place to place

--- Assets/1-Scripts/Human.cs
+++ Assets/1-Scripts/Human.cs
@@ -50,13 +50,31 @@ public class Human : MonoBehaviour
 
     public void moveToRoom(Room room)
     {
-        currentRoom = room;
-        transform.position = room.transform.position;
+        foreach (RoomConnection roomConnection in currentRoom.Pathways)
+        {
+            if (roomConnection.goesToRoom() == room && !roomConnection.IsLocked)
+            {
+                Debug.Log("Moving from " + currentRoom.name + " to " + room.name);
+                transform.position = room.transform.position;
+                checkTraps();
+                checkFears();
+                return;
+            }
+            
+            if (roomConnection.goesToRoom() == room && roomConnection.IsLocked)
+            {
+                Debug.Log("I, " + name + ", cannot move from " + currentRoom.name + " to " + room.name + " because the door is locked");
+                return;
+            }
+        }
+        
+        Debug.Log("I can't go to " + room.name + " because it's not connected to " + currentRoom.name + " where I currently am");
+        
     }
 
     public void checkTraps()
     {
-            foreach (EnvironmentElement environmentElement in currentRoom.EnvironmentElements)
+            foreach (EnvironmentElement environmentElement in currentRoom.getEnvironmentElements())
             {
                 if (environmentElement.hasTrap())
                 {
