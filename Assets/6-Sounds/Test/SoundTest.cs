aeb9ae9fefea9f879a48ddc6f59d75c6c47891c3
lgruene <lgruene@stud.hs-offenburg.de>
2026-03-11 18:24:23 +0100

    Finished Animators for all characters

---
+++ Assets/6-Sounds/Test/SoundTest.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class SoundTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonOnClickSoundTest()
+    {
+        FindObjectOfType<SoundManager>().Play("Event_Rat");
+    }
+}
